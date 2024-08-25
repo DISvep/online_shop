b7838b90e6119c4b435d46274da08181d6de8f75
DISvep <321861bro@gmail.com>
2024-08-25 14:31:52 +0300

    second

--- main.py
+++ main.py
@@ -33,34 +33,44 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 """)
 
-cursor.executemany("""
-INSERT INTO products (product_id, name, category, price) VALUES (?, ?, ?, ?)
-""", [
-    (101, 'Lenovo', 'Ноутбуки', 24000),
-    (102, 'Redmi note 4', 'Смартфони', 5000),
-    (103, 'Samsung galaxy S1', 'Планшети', 10000),
-    (104, "JBL", 'Навушники', 500),
-    (105, "Philips", "Холодильники", 50000)
-])
+# cursor.executemany("""
+# INSERT INTO products (product_id, name, category, price) VALUES (?, ?, ?, ?)
+# """, [
+#     (101, 'Lenovo', 'Ноутбуки', 24000),
+#     (102, 'Redmi note 4', 'Смартфони', 5000),
+#     (103, 'Samsung galaxy S1', 'Планшети', 10000),
+#     (104, "JBL", 'Навушники', 500),
+#     (105, "Philips", "Холодильники", 50000)
+# ])
+#
+# cursor.executemany("""
+# INSERT INTO customers (customer_id, first_name, last_name, email) VALUES (?, ?, ?, ?)
+# """, [
+#     (1, 'Тарас', "Шевченко", "batkonashbandera@gmail.com"),
+#     (2, 'Леся', "Українка", "coolpysmennik@gmail.com"),
+#     (3, 'Віталій', "Семиволос", "wowsoinsane@gmail.com"),
+#     (4, 'Андрій', "Андрійович", "aaaaa8777@gmail.com"),
+#     (5, "Павло", "Трамп", "realtrumpofficialll@gmail.com")
+# ])
+#
+# cursor.executemany("""
+# INSERT INTO orders (order_id, customer_id, product_id, quantity, order_date) VALUES (?, ?, ?, ?, ?)
+# """, [
+#     (201, 2, 101, 1, "25.08.2024"),
+#     (202, 5, 104, 100, "01.01.1488"),
+#     (203, 1, 105, 3, "10.02.2010"),
+#     (204, 3, 102, 2, "24.07.2023"),
+#     (205, 4, 103, 5, "27.09.1945")
+# ])
+#
+# db.commit()
 
-cursor.executemany("""
-INSERT INTO customers (customer_id, first_name, last_name, email) VALUES (?, ?, ?, ?)
-""", [
-    (1, 'Тарас', "Шевченко", "batkonashbandera@gmail.com"),
-    (2, 'Леся', "Українка", "coolpysmennik@gmail.com"),
-    (3, 'Віталій', "Семиволос", "wowsoinsane@gmail.com"),
-    (4, 'Андрій', "Андрійович", "aaaaa8777@gmail.com"),
-    (5, "Павло", "Трамп", "realtrumpofficialll@gmail.com")
-])
-
-cursor.executemany("""
-INSERT INTO orders (order_id, customer_id, product_id, quantity, order_date) VALUES (?, ?, ?, ?, ?)
-""", [
-    (201, 2, 101, 1, "25.08.2024"),
-    (202, 5, 104, 100, "01.01.1488"),
-    (203, 1, 105, 3, "10.02.2010"),
-    (204, 3, 102, 2, "24.07.2023"),
-    (205, 4, 103, 5, "27.09.1945")
-])
-
-db.commit()
+print(
+    cursor.execute("""
+    SELECT
+        SUM(products.price * orders.quantity)
+    FROM
+        orders JOIN products ON orders.product_id = products.product_id
+         
+    """).fetchone()
+)
